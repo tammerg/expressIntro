352c2b7dd497e6195c7e9035192d1531270fd8e7
tammerg <tg385@scarletmail.rutgers.edu>
2016-02-04 19:51:15 -0500

    working on using the express framework to make a login and route to an accounts page after validation

--- script.js
+++ script.js
@@ -3,4 +3,25 @@ var bodyParser = require('body-parser');
 var app = express();
 var PORT = 8080;
 
-//This is the bodyParser middleware
+// This is the bodyParser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/login', function(req, res) {
+  res.sendFile(process.cwd() + '/views/login.html');
+});
+
+app.get('/account', function(req, res) {
+  res.sendFile(process.cwd() + '/views/account.html');
+});
+
+app.post('/login', function(req, res) {
+    // Check to make sure they are an authenticate user
+    // if they are
+    console.log(req.body);
+    res.redirect("/account");
+
+});
+
+app.listen(PORT, function() {
+  console.log('App listening on port %s', PORT);
+});
